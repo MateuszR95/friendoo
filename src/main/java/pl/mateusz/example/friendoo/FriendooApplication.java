c854fb256a70288fc831ae002eb71bdfe81f2f13
Mateusz Rojek <mateuszr2047@gmail.com>
2024-10-08 20:37:38 +0200

    Initial commit

---
+++ src/main/java/pl/mateusz/example/friendoo/FriendooApplication.java
@@ -0,0 +1,13 @@
+package pl.mateusz.example.friendoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendooApplication.class, args);
+    }
+
+}
